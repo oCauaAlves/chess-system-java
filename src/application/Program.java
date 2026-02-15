81ed198d9c9e244b1e710c35fcc9d8ed365e3662
Caua Alves <caua.rodriguesalves2004@gmail.com>
2026-02-15 17:03:43 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,12 @@
 package application;
 
-import application.boardgame.Position;
+import boardgame.Board;
+import boardgame.Position;
 
 public class Program {
     public static void main(String[] args) {
 
-        Position p = new Position(3,5);
-        System.out.println(p);
-
+        Board board = new Board(8,8);
 
     }
 }
