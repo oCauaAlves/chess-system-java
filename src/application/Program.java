eb66e3ce36d1d3d39dfbdffa253aa5deab143246
Caua Alves <caua.rodriguesalves2004@gmail.com>
2026-02-15 16:28:43 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import application.boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
 
-        System.out.println("Chess System!");
+        Position p = new Position(3,5);
+        System.out.println(p);
+
 
     }
 }
