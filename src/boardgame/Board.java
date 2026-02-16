43f544b0a7461792da7f1df7758d5551d70899da
Caua Alves <caua.rodriguesalves2004@gmail.com>
2026-02-16 15:47:24 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -43,7 +43,19 @@ public class Board {
         }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
 
+    public Piece removePiece(Position position) {
+        if (!positionExist(position)) {
+            throw new BoardException("Position not on the board!");
+        }
+        if (piece(position) == null){
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
     }
 
     private boolean positionExist(int row, int column){
